d0bdecfbf62ec724ec8352f92def3dd7ba3e0f48
Unknown <rinaldaj@clarkson.edu>
2016-09-17 22:38:55 -0400

    Voice attempt...robot doesn't speak english

--- voiceNode.py
+++ voiceNode.py
@@ -1,9 +1,47 @@
 import speech_recognition as sr
 from test import audiotests
 
-# obtain audio from the microphone
 r = sr.Recognizer()
 m = sr.Microphone()
+
+def findSpeed(words):
+    if('one' in words):
+        return(.2)
+    elif('three' in words):
+        return(1)
+    else:
+        return(.5)
+
+def control(words,ip):
+    name = words[0]
+    while('end' not in words):
+        lspeed = 0
+        aspeed = 0
+        if('foreward' in words):
+            lspeed = findSpeed(words)
+        elif('backward' in words):
+            lspeed = findSpeed(words) * -1
+        elif('right' in words):
+            aspeed = findSpeed(words) * 2
+        elif('left' in words):
+            aspeed = findSpeed(words) * -2
+        print('linear: ' + str(lspeed) + " rotational: " + str(aspeed))
+        print("Listening...")
+        with m as source: audio = r.listen(source)
+        try:
+            print"Recognizing..."
+            command = r.recognize_sphinx(audio)
+            print("You said: {0}".format(command))
+            words = command.split(' ')
+        except sr.UnknownValueError:
+            print ("Didn't catch that")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+        
+# obtain audio from the microphone
+botIp = {} #used as a dictionary for botIps
+botIp['lou']  = '10.0.0.1'
+botIp['raphael'] = '10.0.1.1'
 
 try:
     print("Quiet please!")
@@ -15,7 +53,11 @@ try:
         try:
             print"Recognizing..."
             command = r.recognize_sphinx(audio)
-            print("You said: {0}".format(value))
+            print("You said: {0}".format(command))
+            words = command.split(' ')
+            if(words[0] == 'lou' or words[0] == 'raphael'):
+                control(words,botIp[words[0]])
+            
         except sr.UnknownValueError:
             print ("Didn't catch that")
         except sr.RequestError as e:
